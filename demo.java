5a4197c0fa2d626fb4cbdf0b6ec03f1b00de5e91
1manas7396@gmail.com <root@ip-172-31-11-182.ap-northeast-1.compute.internal>
2024-12-06 03:58:07 +0000

    branch code

---
+++ demo.java
@@ -0,0 +1,21 @@
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+        int product = num1 * num2;
+        int quotient = num1 / num2;
+        int remainder = num1 % num2;
+
+        System.out.println("Number 1: " + num1);
+        System.out.println("Number 2: " + num2);
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+        System.out.println("Remainder: " + remainder);
+    }
+}
+
